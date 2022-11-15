9894f025ab1a1373ca951bd37f92851ce3fcd8d8
Aether Kerstens <philippekerstens@gmail.com>
2022-11-15 15:47:57 -0800

    Refactored offsets method into its own class

---
+++ lib/offsets.rb
@@ -0,0 +1,16 @@
+class Offsets
+  attr_reader :date, :offsets, :a_offset, :b_offset, :c_offset, :d_offset
+
+  def initialize(date)
+    @date = date
+    @offsets = (date.to_i**2).to_s[-4..-1]
+    @a_offset = offsets[0].to_i
+    @b_offset = offsets[1].to_i
+    @c_offset = offsets[2].to_i
+    @d_offset = offsets[3].to_i
+  end
+
+  def self.current_date
+    Date.today.strftime('%D')
+  end
+end
