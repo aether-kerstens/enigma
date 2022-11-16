78cc5e284c3f99e9d8b9c5d95dd00da15d5c0b5a
Aether Kerstens <philippekerstens@gmail.com>
2022-11-15 16:16:09 -0800

    Refactor test expectations from strings to integers

--- spec/keys_spec.rb
+++ spec/keys_spec.rb
@@ -2,7 +2,7 @@ require './lib/keys'
 
 RSpec.describe Keys do
   before(:each) do
-    @keys = Keys.new("06050")
+    @keys = Keys.new('06050')
   end
 
   it 'exists' do
@@ -10,19 +10,19 @@ RSpec.describe Keys do
   end
 
   it 'a_key is a num' do
-    expect(@keys.a_key).to eq("06")
+    expect(@keys.a_key).to eq(06)
   end
 
   it 'b_key is a num' do
-    expect(@keys.b_key).to eq("60")
+    expect(@keys.b_key).to eq(60)
   end
 
   it 'c_key is a num' do
-    expect(@keys.c_key).to eq("05")
+    expect(@keys.c_key).to eq(05)
   end
 
   it 'a_key is a num' do
-    expect(@keys.d_key).to eq("50")
+    expect(@keys.d_key).to eq(50)
   end
 
 end
